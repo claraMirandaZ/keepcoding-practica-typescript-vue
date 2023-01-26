ea653cd764778b9fad76e674a9776368ab19c609
Clara Miranda Zapata <miranda.zapata@gmail.com>
2023-01-26 22:22:47 +0100

    Fix on vue.config for a better deploy

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/platzifakestore/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/keepcoding-practica-typescript-vue/'
+      : '/',
 };
