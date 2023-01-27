4b32312d41342f1bb95f2019bb591569eff87c72
Clara Miranda Zapata <miranda.zapata@gmail.com>
2023-01-27 19:50:26 +0100

    Structure fix
    Reducir vistas a las requeridas: login, listado de productos, detalle de producto y página de perfil

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/login">Login</router-link> |
     <router-link to="/products">Products</router-link> |
     <router-link to="/detail">Detail</router-link> |
--- src/router/index.ts
+++ src/router/index.ts
@@ -9,18 +9,13 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // Route level code-splitting
     // This generates a separate chunk (about.[hash].js) for this route which is lazy-loaded when the route is visited
     // Lazy loading: no carga los archivos hasta que se visita este path
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/LoginView.vue'),
+      import(/* webpackChunkName: "about" */ '../views/LoginView.vue'),
   },
   {
     path: '/products',
